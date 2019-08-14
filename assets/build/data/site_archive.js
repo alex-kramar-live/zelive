12055bdba1292f30ccbc4e2590b118f1bbac28de
Alex <alex.kramar.live@gmail.com>
2019-08-14 13:29:11 +0300

    Counters fixes

--- assets/build/data/site_archive.js
+++ assets/build/data/site_archive.js
@@ -9,7 +9,7 @@ var site_archive = {
             "is_need_translation": false
         },
         "this_week_violations_count": {
-            "data": 1,
+            "data": 0,
             "is_need_translation": false
         },
         "months_violations_count": {
@@ -42,7 +42,7 @@ var site_archive = {
         },
         "working_progressbar_count": {
             "is_need_translation": true,
-            "counter": "81",
+            "counter": "86",
             "data": "{$working_title} #d {$working_count}",
             "translations": {
                 "working_title": {
@@ -72,7 +72,7 @@ var site_archive = {
         },
         "working_till_end_progressbar_count": {
             "is_need_translation": true,
-            "counter": "1745",
+            "counter": "1740",
             "data": "{$working_till_end_title} #d {$working_till_end_count}",
             "translations": {
                 "working_till_end_title": {
